3af3c5a78830df00095cf31596f6635b3dd60268
sriram98v <sriram.98v@gmail.com>
2022-06-15 12:43:15 -0500

    changed path of config

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', '{{ "/assets/particlesjs-config.json" | url }}', function() {
+particlesJS.load('particles-js', "/assets/particlesjs-config.json", function() {
     console.log('callback - particles.js config loaded');
   });
